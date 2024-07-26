017e2abf612ea3e50e0e7d71fa770d1f5c668a39
Agnieszka Góralczyk <a.j.goralczyk@gmail.com>
2024-07-26 13:12:16 -0400

    header done

--- frontend/src/app/components/Header.tsx
+++ frontend/src/app/components/Header.tsx
@@ -8,24 +8,30 @@ type HeaderProps = {
 export default function Header(props: HeaderProps) {
   const { description, image, title } = props;
   return (
-    <section className="bg-background w-full h-96 relative overflow-hidden">
+    <section className="bg-background w-full relative overflow-hidden">
+      
       {image ? (
         <Image file={image} className="aspect-[2/1] object-cover"/>
       ) : (
+        // TODO what to show when there is no image for category?
         <div className="h-full">
           <p className="text-[400px] text-yellow opacity-30 h-full -translate-y-1/2">{title}</p>
         </div>
       )}
-      <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-white-300/75 to-transparent p-6 md:p-8 lg:p-10">
-        <h1 className="text-3xl font-bold tracking-tight text-secondary sm:text-4xl md:text-5xl">
-          {title}
-        </h1>
-        {description && (
-          <p className="text-xl tracking-tight text-secondary sm:text-2xl md:text-3xl">
-            {description}
-          </p>
-        )}
+      {/* TODO check calculation, something is working incorrectly */}
+      <div className="bg-background2 w-[calc(50%+504px)] lg:pl-[calc(50%-512px)] lg:absolute lg:bottom-0 lg:left-0 lg:right-0 lg:rounded-tr-xl">
+        <div className="p-4 lg:p-6">
+          <h1 className="font-bold text-secondary text-3xl sm:text-4xl">
+            {title}
+          </h1>
+          {description && (
+            <p className="text-sm sm:text-base">
+              {description}
+            </p>
+          )}
+        </div>
       </div>
+
     </section>
   );
 }
